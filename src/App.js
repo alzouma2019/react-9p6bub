7b244b74769e34971dc9b013daae80fa6902e6ff
alzouma2019 <57457748+alzouma2019@users.noreply.github.com>
2022-10-07 17:29:57 +0200

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from 'react';
 import './style.css';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Auth from './component/Auth';
 import 'bootstrap/dist/css/boostrap.min.css';
 
-export default function App() {
-  return <div></div>;
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
