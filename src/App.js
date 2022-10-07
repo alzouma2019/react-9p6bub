85cc4641902705fbc17544983810ac6cf71b7189
alzouma2019 <57457748+alzouma2019@users.noreply.github.com>
2022-10-07 17:25:17 +0200

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './style.css';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/boostrap.min.css';
 
 export default function App() {
